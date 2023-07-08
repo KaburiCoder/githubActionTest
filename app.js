01c6a1043b3981ecdfa93c118c2a55df474820b9
Kaburi <specialemail90@gmail.com>
2023-07-08 18:15:03 +0900

    SERVER환경변수 추가

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+  const ip = process.env.SERVER_IP || 'None';
+  const pwd = process.env.SERVER_PWD || 'None';
   res.send('Hello World!123');
 });
 
